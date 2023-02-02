0b85e27d82d4c0751efdd4b9fd645c0ddd9b568d
thsuekfk2 <thsuekfk2@naver.com>
2023-02-02 23:05:43 +0900

    feat : mvc and template engine

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,8 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
 
 @Controller
 public class HelloController {
@@ -11,5 +13,9 @@ public class HelloController {
         model.addAttribute("data","hello!");
         return "hello";
     }
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model){
+        model.addAttribute("data",name);
+        return "hello-template";
+    }
 }
- 
