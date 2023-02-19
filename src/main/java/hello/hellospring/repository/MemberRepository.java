59a2b11c0ae3f0031e665d050430f6392232c858
thsuekfk2 <thsuekfk2@naver.com>
2023-02-19 17:19:04 +0900

    feat : create member domain, repository

---
+++ src/main/java/hello/hellospring/repository/MemberRepository.java
@@ -0,0 +1,13 @@
+package hello.hellospring.repository;
+
+import hello.hellospring.domain.Member;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface MemberRepository {
+    Member save(Member member); //저장소에 회원 저장
+    Optional<Member> findById(Long id); //id로 회원 찾기
+    Optional<Member> findByName(String name); //name으로 회원 찾기
+    List<Member> findAll(); //저장된 모든 회원 리스트 반환
+}
